46448a8c19eee49908937e892415a77553d77551
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-29 11:05:15 -0500

    liderprobando

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = var.lambda_function_name
   role = aws_iam_role.lambda_exec_role.arn
   handler = "lambda_function.lambda_handler"
-  runtime = "python3.13"
+  runtime = "python3.12"
   filename = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
